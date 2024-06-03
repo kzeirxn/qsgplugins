0705631cdef099d22e03e36e044f912aa933b1ab
Keiron <71524914+uhkeiron@users.noreply.github.com>
2024-06-03 01:12:19 +0100

    Add files via upload

---
+++ kuma/uptime-status-agent.py
@@ -0,0 +1,25 @@
+from discord.ext import commands, tasks
+import aiohttp
+
+HEARTBEAT_URI = "https://status.qservices.cc/api/push/x8Sla8ESG5?status=up&msg=OK&ping=" 
+
+class Uptime_Status_Agent(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.heartbeat.start()
+
+    def cog_unload(self):
+        self.heartbeat.cancel()
+
+    @tasks.loop(seconds=30)
+    async def heartbeat(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(HEARTBEAT_URI) as response:
+                print("Heartbeat Sent")
+
+    @heartbeat.before_loop
+    async def before_heartbeat(self):
+        await self.bot.wait_until_ready()
+
+async def setup(bot):
+    await bot.add_cog(Uptime_Status_Agent(bot))
